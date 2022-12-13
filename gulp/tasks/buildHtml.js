2d7bbccc963c1961439093ed5b28e51110ccec5f
Andrii <iluxor@yandex.ru>
2022-12-13 04:16:47 +0200

    Update buildHtml.js

--- gulp/tasks/buildHtml.js
+++ gulp/tasks/buildHtml.js
@@ -1,9 +1,17 @@
 const gulp = require("gulp");
 const fileinclude = require("gulp-file-include");
-const browserSync = require("browser-sync");
+const beautify = require("gulp-beautify");
 
 const buildHtml = () => {
-  return gulp.src(["app/*.html"]).pipe(fileinclude()).pipe(gulp.dest("dist")).pipe(browserSync.stream());
+  return gulp
+    .src(["app/*.html"])
+    .pipe(
+      fileinclude({
+        prefix: "@",
+      })
+    )
+    .pipe(beautify.html({ indent_size: 2 }))
+    .pipe(gulp.dest("dist"));
 };
 
 module.exports = { buildHtml };
