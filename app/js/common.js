b3806357d97dee2edf40b1f1175f7f7810258e56
Andrey Dudin <iluxor@yandex.ru>
2022-12-13 04:22:07 +0200

    refactor: refactor

--- app/js/common.js
+++ app/js/common.js
@@ -1,10 +1,27 @@
 import $ from "jquery";
-require("select2");
+import Swiper from "swiper";
+import "select2";
 
 $(function () {
   if ($(".js-select").length) {
     $(".js-select").select2({
       minimumResultsForSearch: -1,
+    });
+  }
+
+  if ($(".js-swiper").length) {
+    new Swiper(".js-swiper", {
+      loop: true,
+      pagination: {
+        el: ".swiper-pagination",
+      },
+      navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      },
+      scrollbar: {
+        el: ".swiper-scrollbar",
+      },
     });
   }
 });
