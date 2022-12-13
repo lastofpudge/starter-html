a89481d12453d21b5449f2a425db3909c3f32716
Andrii <iluxor@yandex.ru>
2022-12-13 04:16:07 +0200

    Update assets.js

--- gulp/tasks/assets.js
+++ gulp/tasks/assets.js
@@ -2,11 +2,14 @@ const gulp = require("gulp");
 const imageminfn = require("gulp-imagemin");
 
 const imageMin = () => {
-  return gulp.src(["app/assets/img/**/*"]).pipe(imageminfn());
+  return gulp
+    .src(["app/assets/images/**/*"])
+    .pipe(imageminfn([imageminfn.optipng({ optimizationLevel: 5 }), imageminfn.mozjpeg({ quality: 75, progressive: true })]))
+    .pipe(gulp.dest("dist/assets/images/"));
 };
 
 const imageCopy = () => {
-  return gulp.src(["app/assets/img/**/*"]).pipe(gulp.dest("dist/assets/img/"));
+  return gulp.src(["app/assets/images/**/*"]).pipe(gulp.dest("dist/assets/images/"));
 };
 
 const copyFonts = () => {
