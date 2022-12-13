f4cd8eb4a3c90a535d85dc0f96bee344f142419b
Andrii <iluxor@yandex.ru>
2022-12-13 04:15:19 +0200

    Update gulpfile.babel.js

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,33 +1,42 @@
 const gulp = require("gulp");
 const cleanfn = require("gulp-clean");
 
+const browsersync = require("browser-sync").create();
 const { buildSass } = require("./gulp/tasks/buildSass");
 const { buildJs } = require("./gulp/tasks/buildJs");
 const { buildHtml } = require("./gulp/tasks/buildHtml");
 const { buildSvgSprite } = require("./gulp/tasks/buildSvgSprite");
-const { browsersyncfn } = require("./gulp/tasks/browsersync");
 const { imageMin, imageCopy, copyFonts } = require("./gulp/tasks/assets");
 
-function startwatch() {
-  gulp.watch("app/assets/svg/*.svg", { usePolling: true }, buildSvgSprite);
-  gulp.watch("app/assets/fonts/**/*", { usePolling: true }, copyFonts);
-  gulp.watch("app/sass/**/*.sass", { usePolling: true }, buildSass);
-  gulp.watch(["app/js/common.js"], { usePolling: true }, buildJs);
-  gulp.watch(["app/**/*.html"], { usePolling: true }, buildHtml);
-}
+const browserSync = (done) => {
+  browsersync.init({
+    server: {
+      baseDir: "dist/",
+    },
+    ghostMode: { clicks: false },
+    notify: false,
+    online: true,
+  });
+  done();
+};
 
-function clean(cb) {
+const browserSyncReload = (done) => {
+  browsersync.reload();
+  done();
+};
+
+const startwatch = () => {
+  gulp.watch("app/assets/svg/*.svg", gulp.series(buildSvgSprite, browserSyncReload));
+  gulp.watch("app/assets/fonts/**/*", gulp.series(copyFonts, browserSyncReload));
+  gulp.watch("app/assets/images/**/*", gulp.series(imageCopy, browserSyncReload));
+  gulp.watch("app/sass/**/*.sass", gulp.series(buildSass, browserSyncReload));
+  gulp.watch(["app/js/common.js"], gulp.series(buildJs, browserSyncReload));
+  gulp.watch(["app/**/*.html"], gulp.series(buildHtml, browserSyncReload));
+};
+
+const clean = () => {
   return gulp.src("dist", { allowEmpty: true }).pipe(cleanfn());
-}
+};
 
-exports.default = gulp.series(
-  imageCopy,
-  buildHtml,
-  buildJs,
-  buildSass,
-  copyFonts,
-  buildSvgSprite,
-  gulp.parallel(browsersyncfn, startwatch)
-);
-
-exports.build = gulp.series(clean, imageMin, imageCopy, buildHtml, buildJs, buildSass, copyFonts, buildSvgSprite);
+exports.default = gulp.parallel(startwatch, browserSync);
+exports.build = gulp.series(clean, imageMin, buildHtml, buildJs, buildSass, copyFonts, buildSvgSprite);
