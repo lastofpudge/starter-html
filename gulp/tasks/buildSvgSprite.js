066c65df99a04a5b494f648e67991b766858abcd
Andrii <iluxor@yandex.ru>
2022-12-13 04:17:47 +0200

    Update buildSvgSprite.js

--- gulp/tasks/buildSvgSprite.js
+++ gulp/tasks/buildSvgSprite.js
@@ -1,9 +1,28 @@
 const gulp = require("gulp");
 const svgSprite = require("gulp-svg-sprite");
+const cheerio = require("gulp-cheerio");
+const svgmin = require("gulp-svgmin");
 
 const buildSvgSprite = () => {
   return gulp
     .src("app/assets/svg/*.svg")
+    .pipe(
+      svgmin({
+        js2svg: {
+          pretty: true,
+        },
+      })
+    )
+    .pipe(
+      cheerio({
+        run: function ($) {
+          $("[fill]").removeAttr("fill");
+          $("[stroke]").removeAttr("stroke");
+          $("[style]").removeAttr("style");
+        },
+        parserOptions: { xmlMode: true },
+      })
+    )
     .pipe(
       svgSprite({
         mode: {
